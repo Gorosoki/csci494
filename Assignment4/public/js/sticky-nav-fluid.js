28fae6f46434e6da78af98af68381f299cc3f9a2
Gorosoki <gorosoki@gmail.com>
2015-10-06 00:02:47 -0600

    nav bar added and css updated

---
+++ Assignment4/public/js/sticky-nav-fluid.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var header = document.getElementById("topNav");
+    var foldHeight = header.offsetTop;
+
+    window.addEventListener("scroll", function(event){
+       if (window.pageYOffset < foldHeight) {
+           header.style.position = "relative";
+       }
+       if (window.pageYOffset > foldHeight) {
+           header.style.position = "fixed";
+           header.style.top = "0px";
+       }
+   });
+});
