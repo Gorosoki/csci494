b39bd3b74daf0c43cd63bd325d9b34e7313a9366
Gorosoki <gorosoki@gmail.com>
2015-11-10 16:52:24 -0700

    fixed handlebars issue

--- Assignment5/authprojtest.git/server.js
+++ Assignment5/authprojtest.git/server.js
@@ -12,7 +12,7 @@ var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-var exphbs = require('express3-handlebars');
+var exphbs = require('express-handlebars');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
