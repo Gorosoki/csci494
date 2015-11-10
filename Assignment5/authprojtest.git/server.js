93288b815e19789f45b0b0c5e789ff190a12d7f7
Gorosoki <gorosoki@gmail.com>
2015-11-10 16:01:49 -0700

    updates

--- Assignment5/authprojtest.git/server.js
+++ Assignment5/authprojtest.git/server.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs'); // set up ejs for templating
 //app.set('view engine', 'handlebars');
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch', resave: true, saveUninitialized: true })); // session secret
+app.use(session({ secret: 'ilovelampylamp', resave: true, saveUninitialized: true })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
