c7ee9db921da318d6527975ca1c19c7ee9d7c5c5
Gorosoki <gorosoki@gmail.com>
2015-11-10 16:57:23 -0700

    fixed database url

--- Assignment5/authprojtest.git/config/database.js
+++ Assignment5/authprojtest.git/config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passportDB:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://localhost/passportDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
 
